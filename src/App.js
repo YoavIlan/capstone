cb1873eb99915a1481f353b479c044bf95e3393b
Yoav Ilan <ilan.yoav@gmail.com>
2024-01-22 21:02:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { MUIButton } from './MUIButtons';
+import { Potrace } from './potrace';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [previewSrc, setPreviewSrc] = useState('');
+  const [f, setF] = useState('');
+  const [svg, setSVG] = useState('');
+
+  const handleFileSelect = (event) => {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+      setF(file);
+      console.log('set f')
+    }
+  };
+  
+  const { loadImageFromFile, process, setParameter, getSVG } = Potrace;
+
+  const downloadOutput = (output) => {
+    const blob = new Blob([output], { type: 'image/svg+xml' });
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = 'your_image.svg';
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+
+  const getOutput = () => {
+    console.log('finished process');
+    const output = getSVG(1)
+    setSVG(output);
+    console.log(output);
+    // downloadOutput(output);
+  };
+
+  const convertImage = () => {
+    loadImageFromFile(f);
+    console.log('hit load');
+    process(getOutput);
+  };
+
+  const SVGRenderer = ({ svgString }) => {
+    return (
+      <div dangerouslySetInnerHTML={{ __html: svgString }} />
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleFileSelect} />
+      {previewSrc && <img src={previewSrc} alt="Preview" />}
+      <MUIButton onClick={() => {convertImage()}}>Load me</MUIButton>
+      <SVGRenderer svgString={svg}></SVGRenderer>
     </div>
   );
 }

2caa1394e49f8a65b494208efbd03e3c3bb32412
joshuageorge23 <139290009+joshuageorge23@users.noreply.github.com>
2024-02-06 08:21:58 -0600

    Minor Stylistic Updates and Added Drag & Drop Image Functionality

--- src/App.js
+++ src/App.js
@@ -1,64 +1,82 @@
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import { MUIButton } from './MUIButtons';
 import { Potrace } from './potrace';
-import React, { useState } from 'react';
-
 
 function App() {
-  const [previewSrc, setPreviewSrc] = useState('');
-  const [f, setF] = useState('');
+  const [file, setFile] = useState(null);
   const [svg, setSVG] = useState('');
+  const [isConverted, setIsConverted] = useState(false);
+  const [dragActive, setDragActive] = useState(false);
 
-  const handleFileSelect = (event) => {
-    const fileInput = event.target;
-    const file = fileInput.files[0];
-
-    if (file) {
-      setF(file);
-      console.log('set f')
+  const onFileChange = (event) => {
+    const newFile = event.target.files[0];
+    if (newFile) {
+      setFile(newFile);
+      setIsConverted(false);
     }
   };
-  
-  const { loadImageFromFile, process, setParameter, getSVG } = Potrace;
 
-  const downloadOutput = (output) => {
-    const blob = new Blob([output], { type: 'image/svg+xml' });
-
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = 'your_image.svg';
-
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-  }
-
-  const getOutput = () => {
-    console.log('finished process');
-    const output = getSVG(1)
-    setSVG(output);
-    console.log(output);
-    // downloadOutput(output);
+  const onDragOver = (event) => {
+    event.preventDefault();
+    setDragActive(true);
   };
+
+  const onDragLeave = () => {
+    setDragActive(false);
+  };
+
+  const onDrop = (event) => {
+    event.preventDefault();
+    const newFile = event.dataTransfer.files[0];
+    if (newFile) {
+      setFile(newFile);
+      setIsConverted(false);
+    }
+    setDragActive(false);
+  };
+
+  const { loadImageFromFile, process, getSVG } = Potrace;
 
   const convertImage = () => {
-    loadImageFromFile(f);
-    console.log('hit load');
-    process(getOutput);
+    if (file) {
+      loadImageFromFile(file);
+      process(() => {
+        const output = getSVG(1);
+        setSVG(output);
+        setIsConverted(true);
+      });
+    }
   };
 
-  const SVGRenderer = ({ svgString }) => {
-    return (
-      <div dangerouslySetInnerHTML={{ __html: svgString }} />
-    );
+  const downloadSVG = () => {
+    const blob = new Blob([svg], { type: 'image/svg+xml' });
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = 'converted_image.svg';
+    downloadLink.click();
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileSelect} />
-      {previewSrc && <img src={previewSrc} alt="Preview" />}
-      <MUIButton onClick={() => {convertImage()}}>Load me</MUIButton>
-      <SVGRenderer svgString={svg}></SVGRenderer>
+    <div className="App">
+      <div className={`drag-image ${dragActive ? "active" : ""} ${file ? "file-uploaded" : ""}`}
+           onDragOver={onDragOver}
+           onDragLeave={onDragLeave}
+           onDrop={onDrop}>
+        {!isConverted && !file && (
+          <>
+            <h6>{dragActive ? "Release to Upload File" : "Drag & Drop to Upload File"}</h6>
+            <input type="file" onChange={onFileChange} hidden />
+            <button onClick={() => document.querySelector("input[type=file]").click()}>Choose a file</button>
+          </>
+        )}
+        {file && !isConverted && <img src={URL.createObjectURL(file)} alt="Preview" className="uploaded-image"/>}
+        {isConverted && <div dangerouslySetInnerHTML={{ __html: svg }} className="svg-container"/>}
+      </div>
+      <div className="button-container">
+        {!isConverted && file && <MUIButton onClick={convertImage}>Convert</MUIButton>}
+        {isConverted && <MUIButton onClick={downloadSVG}>Download SVG</MUIButton>}
+      </div>
     </div>
   );
 }
